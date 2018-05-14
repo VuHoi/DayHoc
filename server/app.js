915100ef0963df29422a6799c01145f33371b5c9
hoi <toilahoi007@gmail.com>
2018-05-14 09:41:35 +0700

    Building for production

--- server/app.js
+++ server/app.js
@@ -8,7 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,14 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express hello hello' });
 });
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+ 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
