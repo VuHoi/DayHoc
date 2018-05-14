77bfce0a8dc7b09a52efd8594b43bd64ea885273
hoi <toilahoi007@gmail.com>
2018-05-14 09:28:45 +0700

    Building for production

--- server/app.js
+++ server/app.js
@@ -19,13 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express hello hello' });
 });
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
